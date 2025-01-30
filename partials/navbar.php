fd91937aa608ea842135ca3ed7301dbe97c204bd
Awishka Isuru <awishkaisuru0573@gmail.com>
2025-01-30 23:23:35 +0530

    nav.css

--- partials/navbar.php
+++ partials/navbar.php
@@ -3,11 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel = "style sheet" href="../assets/css/nav.css">
     <title>Document</title>
 </head>
 <body>
 <header>
-    <div>
+    <div class="navbar">
         <ul>
             <li>Home</li>
             <li>find</li>
