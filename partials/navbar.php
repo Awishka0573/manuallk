6670aeff78c756263b2ce59f90a9b449b4060796
Awishka Isuru <awishkaisuru0573@gmail.com>
2025-01-30 23:52:59 +0530

    add css to navbar

--- partials/navbar.php
+++ partials/navbar.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel = "style sheet" href="../assets/css/nav.css">
-    <title>Document</title>
+    <link rel = "stylesheet" href="../assets/css/nav.css">
+    <title>manuallk</title>
 </head>
 <body>
 <header>
     <div class="navbar">
-        <ul>
+        <ul class ="nav_list">
             <li>Home</li>
             <li>find</li>
             <li>Aboutus</li>
