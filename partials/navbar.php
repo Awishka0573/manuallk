1cac1840dc2bc8878edd8ff8ce32a7afa0a60f19
Awishka Isuru <awishkaisuru0573@gmail.com>
2025-01-30 22:11:18 +0530

    navbar

---
+++ partials/navbar.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<header>
+    <div>
+        <ul>
+            <li>Home</li>
+            <li>find</li>
+            <li>Aboutus</li>
+            <li>Contactus</li>
+        </ul>
+    </div>
+    
+</body>
+</html>
+
+</header>
