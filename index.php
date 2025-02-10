ee4fb8e02652ac8449864216d8ed701d6cc4d4bd
Awishka Isuru <awishkaisuru0573@gmail.com>
2025-02-11 01:21:58 +0530

    clear the all errors

--- index.php
+++ index.php
@@ -1,18 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <link rel="stylesheet" href="../manuallk/assets/css/index.css">
-      <link rel="stylesheet" href="../manuallk/assets/css/nav.css">
-      <title>Manuallk</title>
-    
-  </head>
-  <body>
-
-    <?php
-    include ("../manuallk/partials/navbar.php");
-    ?>
-
-    </body>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet"  href="../manuallk/assets/css/nav.css">
+  <link rel="stylesheet"  href="../manuallk/assets/css/index.css">
+  <title>ManualLK</title>
+  <link rel="icon" type="image/png" href="../manuallk/assets/images/logotransp.png">
+</head>
+<body>
+  <?php
+  include('../manuallk/partials/navbar.php');
+  ?>
+  <div class="container">
+    <div class="box">
+      <p>Welcome Home</p>
+      <button class="start-btn">Start Here</button>
+    </div>
+  </div>
+  
+  
+</body>
 </html>
--- partials/navbar.php
+++ partials/navbar.php
@@ -1,15 +1,14 @@
 <header>
-<nav class="navbar">
+    <div class="navbar">
         <div class="logo">
-            <img src="assets\images\logotransp.png" alt="Logo"> 
-            <span>Manuallk</span>
+            <img src="assets\images\logotransp.png" alt="Logo">
+            <p>ManualLK</p>
         </div>
         <ul class="nav-links">
             <li><a href="#">Home</a></li>
             <li><a href="#">Find</a></li>
-            <li><a href="#">About Us</a></li>
-            <li><a href="#">Contact Us</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
         </ul>
-        <div class="hamburger" onclick="toggleMenu()">&#9776;</div>
-    </nav>
+    </div>
 </header>
