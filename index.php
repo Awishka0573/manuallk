bbf4ab0dc2f201973b60858122e0bb11c419791b
Awishka Isuru <awishkaisuru0573@gmail.com>
2025-02-12 23:16:33 +0530

    card and footer add

--- index.php
+++ index.php
@@ -12,13 +12,98 @@
   <?php
   include('../manuallk/partials/navbar.php');
   ?>
-  <div class="container">
+  <div class="containerwelcome">
     <div class="box">
+      <div class="glaz">
       <p>Welcome Home</p>
       <button class="start-btn">Start Here</button>
     </div>
+    </div>
   </div>
-  
-  
+  <div class="wel_container">
+    <div class="welcome">
+      Welcome to ManualLK<br> 
+      Sri Lanka's largest vehicle marketplace. Whether you're looking to buy or sell, we offer a wide range of vehicles at affordable prices.<br>
+      Our platform connects buyers and sellers seamlessly, ensuring a hassle-free experience. Start your journey today and find the perfect vehicle to match your needs.    </div>
+  </div>
+
+  <div class="card_container">
+  <div class="card">
+      <img src="../manuallk/assets/images//ct.jpg" alt="Avatar" style="width: 100%">
+      <div class="container">
+        <h4><b>CT100</b></h4>
+        <p>Bajaj 100cc</p>
+      </div>
+    </div>
+    <div class="card">
+      <img src="../manuallk/assets/images/threeweel.png" alt="Avatar" style="width: 100%">
+      <div class="container">
+        <h4><b>Three Wheeler</b></h4>
+        <p>Bajaj re205</p>
+      </div>
+    </div>
+    <div class="card">
+      <img src="../manuallk/assets/images/alto.png" alt="Avatar" style="width: 100%">
+      <div class="container">
+        <h4><b>Alto</b></h4>
+        <p>Suzuki 800cc</p>
+      </div>
+    </div>
+    <div class="card">
+      <img src="../manuallk/assets/images/kdh.png" alt="Avatar" style="width: 100%">
+      <div class="container">
+        <h4><b>KDH</b></h4>
+        <p>Toyota 2500cc</p>
+      </div>
+    </div>
+  </div>
+
+  <footer class="footer">
+  	 <div class="container">
+  	 	<div class="row">
+  	 		<div class="footer-col">
+  	 			<h4>company</h4>
+  	 			<ul>
+  	 				<li><a href="#">about us</a></li>
+  	 				<li><a href="#">our services</a></li>
+  	 				<li><a href="#">privacy policy</a></li>
+  	 				<li><a href="#">affiliate program</a></li>
+  	 			</ul>
+  	 		</div>
+  	 		<div class="footer-col">
+  	 			<h4>get help</h4>
+  	 			<ul>
+  	 				<li><a href="#">FAQ</a></li>
+  	 				<li><a href="#">shipping</a></li>
+  	 				<li><a href="#">returns</a></li>
+  	 				<li><a href="#">order status</a></li>
+  	 				<li><a href="#">payment options</a></li>
+  	 			</ul>
+  	 		</div>
+  	 		<div class="footer-col">
+  	 			<h4>online shop</h4>
+  	 			<ul>
+  	 				<li><a href="#">watch</a></li>
+  	 				<li><a href="#">bag</a></li>
+  	 				<li><a href="#">shoes</a></li>
+  	 				<li><a href="#">dress</a></li>
+  	 			</ul>
+  	 		</div>
+  	 		<div class="footer-col">
+  	 			<h4>follow us</h4>
+  	 			<div class="social-links">
+  	 				<a href="#"><i class="fab fa-facebook-f"></i></a>
+  	 				<a href="#"><i class="fab fa-twitter"></i></a>
+  	 				<a href="#"><i class="fab fa-instagram"></i></a>
+  	 				<a href="#"><i class="fab fa-linkedin-in"></i></a>
+  	 			</div>
+  	 		</div>
+  	 	</div>
+  	 </div>
+  </footer>
+
+  <script>
+    document.documentElement.style.scrollBehavior = "smooth";
+  </script>
 </body>
 </html>
