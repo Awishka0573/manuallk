4183315ee4abaf90e2c5848ea7186bab4175a935
Awishka Isuru <awishkaisuru0573@gmail.com>
2025-02-17 23:21:25 +0530

    changes

--- pages/contactus.php
+++ pages/contactus.php
@@ -7,6 +7,8 @@
     <link rel="stylesheet"  href="../assets/css/footer.css">
     <link rel="stylesheet"  href="../assets/css/contact.css">
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    <link rel="icon" type="image/png" href="../assets/images/logotransp.png">
+
     <title>Contact Us</title>
 </head>
 <body>
@@ -14,7 +16,7 @@
 <?php
   include('../partials/navbar.php');
 ?>
-<div class="container">
+<div class="container1">
     <div class="box">
         <div class="list">
             <ul class="list_i">
@@ -25,17 +27,17 @@
         </div>
     </div>
     <div class="formdiv">
-        <form action="#" method="post">
-            <label for="fname">Name:</label><br>
-            <input type="text" id="fname" name="fname" required><br><br>
+        <form class="frm" action="#" method="post">
+            <label for="fname" class="name">Name:</label><br>
+            <input type="text"  id="fname" name="fname" size="50" required><br><br>
 
-            <label for="email">Email Address:</label><br>
-            <input type="email" id="email" name="email" required><br><br>
+            <label for="email" class="email">Email Address:</label><br>
+            <input type="email" id="email" name="email" size="50" required><br><br>
 
-            <label for="message">Message:</label><br>
+            <label for="message" class="msg">Message:</label><br>
             <textarea id="message" name="message" rows="4" cols="50" required></textarea><br><br>
 
-            <input type="submit" value="Submit">
+            <input type="submit" class="btn" value="Submit">
         </form>
     </div>
 </div>
