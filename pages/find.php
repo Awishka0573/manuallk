6d749eee21de448b3d4379a7ec9a6fe69669cb09
Awishka Isuru <awishkaisuru0573@gmail.com>
2025-02-22 22:49:30 +0530

    find page created

---
+++ pages/find.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Find</title>
+    <link rel="stylesheet"  href="../assets/css/find.css">
+    <link rel="icon" type="image/png" href="../manuallk/assets/images/logotransp.png">
+</head>
+<body>
+
+<div class="container_search">
+
+ 
+</div>
+    
+</body>
+</html>
