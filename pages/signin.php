834ce4b799ea20f2349585a399e448fd4865b1a2
Awishka Isuru <awishkaisuru0573@gmail.com>
2025-02-27 23:49:10 +0530

    sign in created

---
+++ pages/signin.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign UP</title>
+    <link rel="icon" type="image/png" href="../assets/images/logotransp.png">
+    <link rel="stylesheet"  href="../assets/css/signup.css">
+
+
+</head>
+<body>
+
+<div class="formbg">
+    <div class="sideimg">
+        <h3 class="here">SIGN IN <br>
+        Here</h3>
+    </div>
+    <div class="formdiv">
+            <form class="frm" action="#" method="post">
+               
+                <label for="email" class="email">Email</label><br>
+                <input type="email" id="email" name="email" size="50" required><br><br>
+
+                <label for="password" class="password">Password</label><br>
+                <input type="text"  id="password" name="password" size="50" required><br><br>
+
+                <input type="submit" class="btn" value="Submit">
+            </form>
+    </div>
+    
+</div>
+    
+</body>
+</html>
